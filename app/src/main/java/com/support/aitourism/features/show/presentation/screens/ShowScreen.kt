2c9d6c086217acd7af7498c29d1fccb83d7645f5
mhammadgammal <mhammadgamal17@gmail.com>
2024-07-31 11:06:41 +0300

    prepare show screen that will display selected photos

---
+++ app/src/main/java/com/support/aitourism/features/show/presentation/screens/ShowScreen.kt
@@ -0,0 +1,9 @@
+package com.support.aitourism.features.show.presentation.screens
+
+import android.net.Uri
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ShowScreen(uri: Uri?) {
+
+}

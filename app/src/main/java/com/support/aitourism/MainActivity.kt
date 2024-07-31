2f7b9ae3182c54dc16b75d79ef9275d5fa214ae7
mhammadgammal <mhammadgamal17@gmail.com>
2024-07-31 13:55:20 +0300

    implement an AsyncImage widget to load the photos from the passed URI

--- app/src/main/java/com/support/aitourism/MainActivity.kt
+++ app/src/main/java/com/support/aitourism/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.support.aitourism
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.support.aitourism.core.utils.BaseActivity
+import com.support.aitourism.core.utils.launchPicker
 import com.support.aitourism.features.authentication.presentation.ui.login.LoginScreen
 import com.support.aitourism.features.home.presentation.ui.HomeScreen
+import com.support.aitourism.features.show.presentation.screens.ShowScreen
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.serialization.Serializable
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
@@ -34,10 +39,32 @@ class MainActivity : BaseActivity() {
             })
 
             composable("home", content = {
-                HomeScreen {
-                    handleCameraPermission()
-                }
+                HomeScreen(openCamera = {
+                    handleCameraPermission {
+                        Log.d(TAG, "Navigation from camera: $it")
+
+                        navController.navigate(ShowImage(uri = it))
+                    }
+                }, openGallery = {
+                    return@HomeScreen launchPicker {
+                        Log.d(TAG, "Navigation from picker: $it")
+
+                        navController.navigate(ShowImage(uri = it.toString()))
+                    }
+                })
             })
+
+            composable<ShowImage> {
+                val showImage = it.toRoute<ShowImage>()
+                ShowScreen(uri = showImage.uri)
+            }
         }
     }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
 }
+
+@Serializable
+data class ShowImage(val uri: String)
--- app/src/main/java/com/support/aitourism/features/show/presentation/screens/ShowScreen.kt
+++ app/src/main/java/com/support/aitourism/features/show/presentation/screens/ShowScreen.kt
@@ -1,9 +1,25 @@
 package com.support.aitourism.features.show.presentation.screens
 
 import android.net.Uri
+import android.util.Log
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 @Composable
-fun ShowScreen(uri: Uri?) {
+fun ShowScreen(uri: String?) {
+    Log.d(TAG, "ShowScreen: Uri in ShowScreen: $uri")
 
+    AsyncImage(
+        model = uri,
+        contentDescription = null,
+        modifier = Modifier
+            .size(200.dp),
+        contentScale = ContentScale.Crop
+    )
 }
+
+const val TAG = "ShowScreen"

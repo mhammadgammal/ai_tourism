fc35eaeccbfd1fe48abd363ec41b9ed451f7b362
mhammadgammal <mhammadgamal17@gmail.com>
2024-07-31 11:05:32 +0300

    remove multi dex support

--- app/src/main/java/com/support/aitourism/AiTourismApp.kt
+++ app/src/main/java/com/support/aitourism/AiTourismApp.kt
@@ -2,13 +2,12 @@ package com.support.aitourism
 
 import android.app.Application
 import android.content.Context
-import androidx.multidex.MultiDex
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class AiTourismApp : Application() {
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
-        MultiDex.install(this)
+
     }
 }

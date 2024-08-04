2f05adf2d05070425a7be2c367c05efe0464e6d2
mhammadgammal <mhammadgamal17@gmail.com>
2024-08-04 08:23:21 +0300

    enable edge to edge feature

--- app/src/main/java/com/support/aitourism/MainActivity.kt
+++ app/src/main/java/com/support/aitourism/MainActivity.kt
@@ -3,6 +3,7 @@ package com.support.aitourism
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
@@ -23,7 +24,7 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-//        enableEdgeToEdge()
+        enableEdgeToEdge()
         setContent {
             Navigation()
         }
